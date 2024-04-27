2ea953635ea013809a820df094c52fdce2a196c9
Tygssgk <162098950+Tygssgk@users.noreply.github.com>
2024-04-27 09:36:13 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'bac5f76b-a181-4077-8921-ce78ebcf80c3';
 
 const proxyIPs = ['35.219.50.99'];
 
